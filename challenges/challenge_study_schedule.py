5dad5d81eb5a5b14ab3cd3ddb980a4e2d62d03fb
pedrohassen <pedro_hasse@yahoo.com.br>
2023-04-25 14:49:56 -0300

    feat: 🦾 Requisito 1 ok

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,9 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aquii."""
-    raise NotImplementedError
+    try:
+        return len([
+            student_login
+            for student_login, student_logout in permanence_period
+            if student_login <= target_time <= student_logout
+        ])
+    except TypeError:
+        return None
