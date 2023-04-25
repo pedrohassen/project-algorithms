4c8640452734d1c323cac710de658322e57ac956
pedrohassen <pedro_hasse@yahoo.com.br>
2023-04-25 17:51:08 -0300

    feat: 🦾 Requisito 5 ok

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,14 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    nums.sort()
+
+    for num in range(len(nums)):
+        if (
+            num + 1 >= len(nums)
+            or not isinstance(nums[num], int)
+            or not isinstance(nums[num + 1], int)
+            or nums[num] < 0
+        ):
+            return False
+        if nums[num] == nums[num + 1]:
+            return nums[num]
+    return False
