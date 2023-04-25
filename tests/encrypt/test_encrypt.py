b56c417b92b8c57c218233c010ef40db616071fe
pedrohassen <pedro_hasse@yahoo.com.br>
2023-04-25 15:33:48 -0300

    test: 🧪 Requisito 2 ok

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,9 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(TypeError):
+        encrypt_message(3, 'abcdef')
+    assert encrypt_message("abcdef", 2) == "fedc_ba"
+    assert encrypt_message("carro", 9) == "orrac"
